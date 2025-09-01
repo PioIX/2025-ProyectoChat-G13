980d64a43aaaa14d6d8413094e9a9e907b70265c
ffacusuarez <fsuarez@pioix.edu.ar>
2025-09-01 08:48:17 -0300

    cambios

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,8 +1,18 @@
 "use client"
 
+import Button from "@/components/Button";
+
 
 export default function Home() {
   return (
-    <p>Putri pedazo de puto</p>
+    <div className="home-container">
+      <h1>Chatsites</h1>
+      <h2>Tu mejor lugar para comunicarte</h2>
+      <div className="entrar-home">
+        <p>Empieza a chatear ya</p>
+        <Button text="ChatSites"></Button>
+      </div>
+      <p className="footer-home"></p>
+    </div>
   );
 }
