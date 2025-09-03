c9b130fe7cc8682bd78a1bfe51452088e027c1be
ffacusuarez <fsuarez@pioix.edu.ar>
2025-09-03 00:09:43 -0300

    holi

--- front/src/app/(routes)/chat/page.js
+++ front/src/app/(routes)/chat/page.js
@@ -1,7 +1,10 @@
 "use client"
 
+import Button from "@/components/Button"
+import Input from "@/components/Input"
 import { useRouter } from "next/navigation"
 import { use, useEffect } from "react"
+import  "./chat.styles.css"
 
 export default function Chat() {
 
@@ -33,7 +36,35 @@ export default function Chat() {
 
     return (
         <div className="screen-device">
-            <h1>Bienvenido al chat</h1>
+            <div className="sidebar">
+                {/* aca deberia ir el buscar, el nuevo chat, y esas cosas y abajo una lista de todos los contactos (otro div claramente) */}
+                <div className="sidebar-header">
+                    <Input type="text" placeholder="Buscar..." />
+                    <Button className="new-chat" text="+"/>
+                    
+                </div>
+                <div className="contact-list">
+                    {/* componente contacto */}  aaa soy un contacto (UsuariosxChat id)
+                </div>
+            </div>
+            <div className="chat-device">
+                <div className="header-chat">
+                    <div className="avatar">
+                        {/* aca va componente contacto  FOTO*/}
+                    </div>
+                    {/* COMPONENTE NAME ACA */} NOMBRE
+                </div>
+                <div className="historial">
+                    {/* aca va chat / historial */}
+                    <div className="message received">{ /* aca va componente mensaje */} HOLA SOY UN MENSAJE</div>
+                    <div className="message sent">{ /* aca va componente mensaje */} HOLA SOY UN MENSAJE</div>
+                </div>
+                <div className="user-interface">
+                    {/* // aca va input de mensajes  */}
+                    <Input type="text" placeholder="Escribe un mensaje..." />
+                    <Button className="send-message" text="Enviar Mensaje"/>
+                </div>                
+            </div>
         </div>
     )
 }
