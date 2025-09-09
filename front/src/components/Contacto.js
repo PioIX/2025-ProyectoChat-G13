9791bcafbe9708782efd68269f670a7d5252a717
agusputrino <aputrino@pioix.edu.ar>
2025-09-09 08:42:41 -0300

    Update Contacto.js

--- front/src/components/Contacto.js
+++ front/src/components/Contacto.js
@@ -31,7 +31,7 @@ export default function Contacto({ onSelectContact = () => {} }) {
                         key={contacto.id_chat} 
                         className={styles.contactItem} 
                         onClick={() => handleClick(contacto)}>
-                            <Imagen 
+                            <Imagen
                                 src={contacto.grupo ? contacto.foto : contacto.foto_perfil}
                                 alt={"Foto de: " + (contacto.grupo ? contacto.nom_grupo : contacto.nombre)}
                                 className={styles.contactImg}
